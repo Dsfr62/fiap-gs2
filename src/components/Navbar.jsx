2861ec531fc974b1f576aaf22ed955441f42beae
Eduardo Sávio <saviodudu79@gmail.com>
2023-11-20 15:10:25 -0300

    iniciando estilização da barra de navegação

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,30 @@
-import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Link, Outlet, useHref } from "react-router-dom";
 import { routes } from "../routes";
+import "../styles/navbar.css";
 
 const Navbar = () => {
+  const pathname = useHref();
+  const [openNav, setOpenNav] = useState(false);
+
   return (
     <>
-      <nav>
-        <h3>Logo</h3>
-        <ul>
+      <nav className="navbar">
+        <h3 className="navbar-brand">FIAP Telemedicina</h3>
+
+        <ul className={`${openNav ? "navbar-open" : "navbar-menu"}`}>
           {routes.map((route) => (
             <li key={route.id}>
-              <Link to={route.path}>{route.id}</Link>
+              <Link
+                to={route.path}
+                className={`${
+                  pathname == route.path
+                    ? "navbar-item-selected"
+                    : "navbar-item"
+                }`}
+              >
+                {route.id}
+              </Link>
             </li>
           ))}
         </ul>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import AppRoutes from "./routes";
-import Navbar from "./components/Navbar";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
