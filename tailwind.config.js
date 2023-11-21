3b2fabc1b9a487361549aa99545d592c428dfe25
Eduardo Sávio <saviodudu79@gmail.com>
2023-11-21 20:49:53 -0300

    cores em tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-default": "#ec6e0a",
+        "secondary-default": "#00a9b3",
+        "gray-default": "#f3f2f2",
+        "gray-dark": "#eeeded",
+      },
+    },
   },
   plugins: [],
 };
