6facf122f04cbe75cad9fd978b5c22623a623cb9
mariconpe <marinapereira.ecp@gmail.com>
2023-11-23 19:33:30 -0300

    refactor: altera modo de passar mask image de classe para inline

--- src/components/Section/Image.jsx
+++ src/components/Section/Image.jsx
@@ -1,9 +1,23 @@
 import { twMerge } from "tailwind-merge";
 
-export const SectionImage = ({ src, ...rest }) => {
+export const SectionImage = ({ src, maskImageFileName, ...rest }) => {
+  const maskImageUrl = `url("../../assets/svgs/${maskImageFileName}.svg")`;
   return (
     <div className="relative">
-      <img style={{}} className={twMerge("w-full h-auto object-cover", rest.className)} src={src} />
+      <img
+        style={
+          maskImageFileName
+            ? {
+                maskImage: maskImageUrl,
+                WebkitMaskImage: maskImageUrl,
+                maskSize: "cover",
+                WebkitMaskSize: "cover",
+              }
+            : {}
+        }
+        className={twMerge("w-full h-auto object-cover", rest.className)}
+        src={src}
+      />
     </div>
   );
 };
