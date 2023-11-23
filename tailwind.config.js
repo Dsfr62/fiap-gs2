20ce8b6ff062399aacecc566f0f7cd7a4384097c
mariconpe <marinapereira.ecp@gmail.com>
2023-11-23 09:11:11 -0300

    feature: adiciona fonte e estilização no tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/styles/core/*.css"],
   theme: {
     extend: {
       colors: {
         "primary-default": "#ec6e0a",
         "secondary-default": "#00a9b3",
+        "secondary-dark": "#0b3558",
+        "secondary-light": "#476788",
         "gray-default": "#f3f2f2",
         "gray-dark": "#eeeded",
       },
     },
+    fontFamily: {
+      sans: ["Gilroy-Medium"],
+    },
   },
-  plugins: [],
+  plugins: [{ "postcss-import": {} }],
 };
